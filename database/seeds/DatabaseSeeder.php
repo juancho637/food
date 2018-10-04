220246909e58a116c7b5359c33c00923b644671a
juancho637 <juancho637@gmail.com>
2018-10-04 16:58:03 -0500

    refactor de los seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Company;
+use App\User;
+use App\Branch;
+use App\Product;
+use App\Client;
+use App\Reservation;
+use Caffeinated\Shinobi\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +19,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        //Permission::trucate();
+        Company::truncate();
+        User::truncate();
+        Branch::truncate();
+        Product::truncate();
+        Client::truncate();
+        Reservation::truncate();
+
+        Permission::flushEventListeners();
+        Company::flushEventListeners();
+        User::flushEventListeners();
+        Branch::flushEventListeners();
+        Product::flushEventListeners();
+        Client::flushEventListeners();
+        Reservation::flushEventListeners();
+
         $this->call(PermissionsTableSeeder::class);
         $this->call(CompaniesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
